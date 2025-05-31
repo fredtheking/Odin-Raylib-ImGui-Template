99b4c92da4b9a1dd2fed73285a774d3b528ec145
FredTheKing <electromaster469@gmail.com>
2025-05-31 15:41:49 +0300

    bat

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+
+mkdir bin 2>nul 
+set project_name="app"
+
+odin build src -out:bin/%project_name%.exe
+if errorlevel 1 (
+    echo Build failed.
+    exit /b 1
+)
+
+cd bin
+.\%project_name%
